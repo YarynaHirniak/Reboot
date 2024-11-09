c7309059d1a55078bef241b6f052e057156acd36
YarynaHirniak <hirniak.pn@ucu.edu.ua>
2024-11-09 10:00:00 +0200

    all code

--- Reboot/demo/src/main/java/flower/store/FlowerRepository.java
+++
@@ -1,9 +0,0 @@
-package flower.store;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FlowerRepository extends JpaRepository<Flower, Integer> {
-
-}
